a4ab71094625d296e735ecf9dc592b610ba5b92e
Akash Yadav <91262354+akash701752@users.noreply.github.com>
2021-10-07 10:55:27 +0530

    Update tpractical5.cpp

--- tpractical5.cpp
+++ tpractical5.cpp
@@ -60,7 +60,95 @@ void bool_func()
 #include<bits/stdc++.h>
 3
 #include<iostream>
+4 // Task To print Datatypes and their size 
+2
+#include<bits/stdc++.h>
+3
+#include<iostream>
 4
+#include<string>
+5
+using namespace std ;
+6
+void char_func()
+7
+{
+8
+  char a = 'A' ;
+9
+  cout<< a << endl ;
+10
+  cout<< sizeof(a) << endl ;
+11
+  cout<< sizeof(char) << endl ;
+12
+}
+13
+void int_fnc()
+14
+{
+15
+  int  a = 786 ;
+16
+  cout<< a << endl ;
+17
+  cout<< sizeof(a) << endl ;
+18
+  cout<< sizeof(int) << endl ;
+19
+}
+20
+void float_fnc()
+21
+{
+22
+  float a = 7864.985 ;
+23
+  cout<< a << endl ;
+24
+  cout<< sizeof(a) << endl ;
+25
+  cout<< sizeof(float) << endl ;
+26
+}
+27
+void short_func()
+28
+{
+29
+  short a = 2002 ;
+30
+  cout<< a << endl ;
+31
+  cout<< sizeof(a) << endl ;
+32
+  cout<< sizeof(short) << endl ;
+33
+}
+34
+void long_func()
+35
+{
+36
+  long  a = 826522 ;
+37
+  cout<< a << endl ;
+38
+  cout<< sizeof(a) << endl ;
+39
+  cout<< sizeof(long) << endl ;
+40
+}
+41
+void longdouble_func()
+42
+{
+43
+  long double a = 826522 ;
+44
+  cout<< a << endl ;
+45
+  cout<< sizeof(a) << endl ;
 #include<string>
 5
 using namespace std ;
