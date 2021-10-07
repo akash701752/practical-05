b101d9b98f4847b6199fd37971635a56b7489406
Akash Yadav <91262354+akash701752@users.noreply.github.com>
2021-10-07 22:40:34 +0530

    Update tpractical5.cpp

--- tpractical5.cpp
+++ tpractical5.cpp
@@ -1,338 +1,50 @@
  // Task To print Datatypes and their size 
-#include<bits/stdc++.h>
 #include<iostream>
-#include<string>
-using namespace std ;
-void char_func()
-{
-  char a = 'A' ;
-  cout<< a << endl ;
-  cout<< sizeof(a) << endl ;
-  cout<< sizeof(char) << endl ;
-}
-void int_func()
-{
-  int  a = 786 ;
-  cout<< a << endl ;
-  cout<< sizeof(a) << endl ;
-  cout<< sizeof(int) << endl ;
-}
-void float_func()
-{
-  float a = 7864.985 ;
-  cout<< a << endl ;
-  cout<< sizeof(a) << endl ;
-  cout<< sizeof(float) << endl ;
-}
-void short_func()
-{
-  short a = 2002 ;
-  cout<< a << endl ;
-  cout<< sizeof(a) << endl ;
-  cout<< sizeof(short) << endl ;
-}
-void long_func()
-{
-  long  a = 826522 ;
-  cout<< a << endl ;
-  cout<< sizeof(a) << endl ;
-  cout<< sizeof(long) << endl ;
-}
-void longdouble_func()
-{
-  long double a = 826522 ;
-  cout<< a << endl ;
-  cout<< sizeof(a) << endl ;
-  cout<< sizeof(long double) << endl ;
-}
-/*void widechar_func()
-{
-  wchar_t  a = L '0' ;
-  cout<< a << endl ;
-  cout<< sizeof(a) << endl ;
-  cout<< sizeof(wchar_t) << endl ;
-}*/
-void bool_func()
-{
-  bool a = true ;
-  cout<< a << endl ; // Task To print Datatypes and their size 
-2
-#include<bits/stdc++.h>
-3
-#include<iostream>
-4 // Task To print Datatypes and their size 
-2
-#include<bits/stdc++.h>
-3
-#include<iostream>
-4
-#include<string>
-5
-using namespace std ;
-6
-void char_func()
-7
-{
-8
-  char a = 'A' ;
-9
-  cout<< a << endl ;
-10
-  cout<< sizeof(a) << endl ;
-11
-  cout<< sizeof(char) << endl ;
-12
-}
-13
-void int_fnc()
-14
-{
-15
-  int  a = 786 ;
-16
-  cout<< a << endl ;
-17
-  cout<< sizeof(a) << endl ;
-18
-  cout<< sizeof(int) << endl ;
-19
-}
-20
-void float_fnc()
-21
-{
-22
-  float a = 7864.985 ;
-23
-  cout<< a << endl ;
-24
-  cout<< sizeof(a) << endl ;
-25
-  cout<< sizeof(float) << endl ;
-26
-}
-27
-void short_func()
-28
-{
-29
-  short a = 2002 ;
-30
-  cout<< a << endl ;
-31
-  cout<< sizeof(a) << endl ;
-32
-  cout<< sizeof(short) << endl ;
-33
-}
-34
-void long_func()
-35
-{
-36
-  long  a = 826522 ;
-37
-  cout<< a << endl ;
-38
-  cout<< sizeof(a) << endl ;
-39
-  cout<< sizeof(long) << endl ;
-40
-}
-41
-void longdouble_func()
-42
-{
-43
-  long double a = 826522 ;
-44
-  cout<< a << endl ;
-45
-  cout<< sizeof(a) << endl ;
-#include<string>
-5
-using namespace std ;
-6
-void char_func()
-7
-{
-8
-  char a = 'A' ;
-9
-  cout<< a << endl ;
-10
-  cout<< sizeof(a) << endl ;
-11
-  cout<< sizeof(char) << endl ;
-12
-}
-13
-void int_fnc()
-14
-{
-15
-  int  a = 786 ;
-16
-  cout<< a << endl ;
-17
-  cout<< sizeof(a) << endl ;
-18
-  cout<< sizeof(int) << endl ;
-19
-}
-20
-void float_fnc()
-21
-{
-22
-  float a = 7864.985 ;
-23
-  cout<< a << endl ;
-24
-  cout<< sizeof(a) << endl ;
-25
-  cout<< sizeof(float) << endl ;
-26
-}
-27
-void short_func()
-28
-{
-29
-  short a = 2002 ;
-30
-  cout<< a << endl ;
-31
-  cout<< sizeof(a) << endl ;
-32
-  cout<< sizeof(short) << endl ;
-33
-}
-34
-void long_func()
-35
-{
-36
-  long  a = 826522 ;
-37
-  cout<< a << endl ;
-38
-  cout<< sizeof(a) << endl ;
-39
-  cout<< sizeof(long) << endl ;
-40
-}
-41
-void longdouble_func()
-42
-{
-43
-  long double a = 826522 ;
-44 // Task To print Datatypes and their size 
-2
-#include<bits/stdc++.h>
-3
-#include<iostream>
-4
-#include<string>
-5
-using namespace std ;
-6
-void char_func()
-7
-{
-8
-  char a = 'A' ;
-9
-  cout<< a << endl ;
-10
-  cout<< sizeof(a) << endl ;
-11
-  cout<< sizeof(char) << endl ;
-12
-}
-13
-void int_fnc()
-14
-{
-15
-  int  a = 786 ;
-16
-  cout<< a << endl ;
-17
-  cout<< sizeof(a) << endl ;
-18
-  cout<< sizeof(int) << endl ;
-19
-}
-20
-void float_fnc()
-21
-{
-22
-  float a = 7864.985 ;
-23
-  cout<< a << endl ;
-24
-  cout<< sizeof(a) << endl ;
-25
-  cout<< sizeof(float) << endl ;
-26
-}
-27
-void short_func()
-28
-{
-29
-  short a = 2002 ;
-30
-  cout<< a << endl ;
-31
-  cout<< sizeof(a) << endl ;
-32
-  cout<< sizeof(short) << endl ;
-33
-}
-34
-void long_func()
-35
-{
-36
-  long  a = 826522 ;
-37
-  cout<< a << endl ;
-38
-  cout<< sizeof(a) << endl ;
-39
-  cout<< sizeof(long) << endl ;
-40
-}
-41
-void longdouble_func()
-42
-{
-43
-  long double a = 826522 ;
-44
-  cout<< a << endl ;
-45
-  cout<< sizeof(a) << endl ;
-  cout<< a << endl ;
-45
-  cout<< sizeof(a) << endl ;
-  cout<< sizeof(a) << endl ;
-  cout<< sizeof(bool) << endl ;
-}
 
-int main() //Task 
+using namespace std ;
+
+int main() 
 {
-  cout<<"char :"<< char_func() ;
-  cout<<"int  :"<<int_func();
-  cout<<"float :"<<float_func();
-  cout<<"short :"<<short_func();
-  cout<<"long :"<<long_func();
-  cout<<"log double :"<<longdouble_func();
-  //cout<<"wide char :"<<widechar_func();
-  cout<<"bool :"<<bool_func()
+   // Printing Value of Datatypes and their Value , size of variable and Size of Datatype Respectively
+   cout<< "Character "<< endl ;
+    char a = 'A' ;
+  cout<< a << endl ;
+  cout<< sizeof(a) << endl ;
+  cout<< sizeof(char) << endl ;
+  cout<< "Integer "<< endl ;
+  int  b = 7867;
+  cout<< b << endl ;
+  cout<< sizeof(b) << endl ;
+  cout<< sizeof(int) << endl ;
+  cout<< "Float"<< endl ;
+  float c = 7864.985 ;
+  cout<< c << endl ;
+  cout<< sizeof(c) << endl ;
+  cout<< sizeof(float) << endl ;
+  cout<< "Short"<< endl ;
+    short d = 2002 ;
+  cout<< d << endl ;
+  cout<< sizeof(d) << endl ;
+  cout<< sizeof(short) << endl ;
+  cout<< "Long"<< endl ;
+  long  e = 826522 ;
+  cout<< e << endl ;
+  cout<< sizeof(e) << endl ;
+  cout<< sizeof(long) << endl ;
+  cout<< "Long double"<< endl ;
+  long double f = 8226522 ;
+  cout<< f << endl ;
+  cout<< sizeof(f) << endl ;
+  cout<< sizeof(long double) << endl ;
+  cout<< "Bool"<< endl ;
+   bool h = true ;
+  cout<< h << endl ; 
+  cout<< sizeof(h) << endl ;
+  cout<< sizeof(bool) << endl ;
+  cout<< "Wide Char"<< endl ;
+  wchar_t w  = L'A';
+  cout<< w << endl ;
+  cout<< sizeof(w) << endl ;
+  cout<< sizeof(wchar_t) << endl ;
   return 0 ;
 }
